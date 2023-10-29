9aed58b4dfbcc9e944c9d410c063221d79bbb98f
leesw9501 <35837505+leesw9501@users.noreply.github.com>
2023-10-30 01:39:57 +0900

    Update login.js

--- week10/routes/login.js
+++ week10/routes/login.js
@@ -3,32 +3,32 @@ import { selectSql } from "../database/sql";
 
 const router = express.Router();
 
-router.get('/', (req, res) =>{
+router.get('/', (req, res) => {
     res.render('login');
 });
 
-router.post('/', async (req, res)=>{
+router.post('/', async (req, res) => {
     const vars = req.body;
     const users = await selectSql.getUser();
 
-    users.map((user)=>{
+    users.map((user) => {
         console.log('ID :', user.Id);
-        if(vars.id === user.Id && vars.password === user.Password){
+        if (vars.id === user.Id && vars.password === user.Password) {
             console.log('login success!');
-            req.session.user = {id:user.Id, role:user.Role, checkLogin:true};
+            req.session.user = { id: user.Id, role: user.Role, checkLogin: true };
         }
     });
-    
-    if(req.session.user.checkLogin && req.session.user.role === 'super'){
-        res.redirect('/delete');
-    } else if(req.session.user.checkLogin && req.session.user.role === 'student'){
-        res.redirect('/select');
-    } else{
+
+    if (req.session.user == undefined) {
         console.log('login failed!');
         res.send(`<script>
                     alert('login failed!');
                     location.href='/';
                 </script>`)
+    } else if (req.session.user.checkLogin && req.session.user.role === 'super') {
+        res.redirect('/delete');
+    } else if (req.session.user.checkLogin && req.session.user.role === 'student') {
+        res.redirect('/select');
     }
 });
 
